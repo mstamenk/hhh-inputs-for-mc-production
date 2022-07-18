2f56d778549b85d1694bbe22483a6d88986300fe
Marko Stamenkovic <marko.stamenkovic@cern.ch>
2022-07-18 11:26:47 -0400

    Updating fragment to add concurrent config as suggested by Wei

--- fragments/fragment_HHHTo4b2g.py
+++ fragments/fragment_HHHTo4b2g.py
@@ -8,6 +8,7 @@ externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
     args = cms.vstring('/isilon/data/users/mstamenk/eos-triple-h/gridpacks/GF_HHH_SM_c3_0_d4_0_no_b_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz'),
     nEvents = cms.untracked.uint32(5000),
     numberOfParameters = cms.uint32(1),
+    generateConcurrently = cms.untracked.bool(True),
     outputFile = cms.string('cmsgrid_final.lhe'),
     scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
 )
